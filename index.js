4ab6d21d8cf4cb1160edd4227d6dd8581c9d4c9b
James John <106777825+Ojaay96@users.noreply.github.com>
2025-06-14 00:38:09 +0100

    updated yml files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('node:http');
 const querystring = require('node:querystring');
 
-const hostname = '0.0.0.0';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
